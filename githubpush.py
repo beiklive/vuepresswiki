e5c94244912a94464291bb96bd65f5710561b877
byeaxj <Jie.Dong@aptiv.com>
2024-05-22 14:11:38 +0800

    🚀 提交者：byeaxj <Jie.Dong@aptiv.com>
    📝 修改内容：
    - 更新了 `.github/ISSUE_TEMPLATE` 目录下的 `bug_report.md`、`join_case.md` 和 `question.md` 文件。
    - 修改了 `.github/workflows` 目录下的 `baiduPush.yml`、`ci.yml`、`gtalkcomment.yml` 以及 `server deploy.yml` 工作流程配置文件。
    - 更新了 `.gitignore` 文件，用于忽略不需要跟踪的文件。
    - 对 `LICENSE` 和 `README.MD` 文件进行了修改。
    - 修改了 `baiduPush.sh` 脚本。
    - 更新了 `beiklive` 目录下的多个 VuePress 配置文件和增强应用脚本。
    - 修改了 `beiklive` 目录下的多个 Markdown 文档，包括归档页面、分类页面、标签页面以及 Godot 笔记和 Linux 笔记等。
    - 更新了 `deploy.sh` 和 `githubpush.py` 脚本。
    - 修改了 `ob` 目录下的多个模板文件。
    - 更新了 `package.json` 文件，可能涉及到项目依赖的更新。
    - 修改了 `tools` 目录下的脚本文件，包括新文章生成器、百度站长推送和自动创建评论的脚本。
    - 更新了 `utils` 目录下的多个 JavaScript 工具文件。
    - 修改了 `vdoing` 目录下的 Vue 组件、配置文件、样式文件和模板文件。
    - 更新了 `vercel.json` 配置文件，可能涉及到部署设置的更改。
    
    请注意，`.obsidian/` 目录下的所有修改已被忽略，不包含在本次提交中。

--- githubpush.py
+++ githubpush.py
@@ -1,4 +1,5 @@
 import subprocess
+from openai import OpenAI
 
 def get_git_user_info():
     # 获取用户姓名和邮箱
@@ -18,6 +19,39 @@ def format_commit_message(user_name, user_email, diff_info):
     commit_message += diff_info
     return commit_message
 
+
+def kimi_robot(commit_message):
+ 
+    client = OpenAI(
+        api_key = "sk-QWBokCyfC7eGQ5DGh9aHRgz18vS4mgpZsztEQhvu1zwXGRij",
+        base_url = "https://api.moonshot.cn/v1",
+    )
+
+    sys_msg = """
+    请你作为AI助手，根据提供的Git提交变更摘要，使用中文润色成一份详略得当的提交信息并添加恰当的emoji表情。
+    要求如下
+    1. .obsidian/ 下的所有修改请忽略不要润色
+    2. ？？ 开头的所有修改请忽略不要润色
+    3. 不要出现可能、应该之类推测性质的词汇
+
+    """
+
+    user_msg = commit_message
+
+    completion = client.chat.completions.create(
+        model = "moonshot-v1-8k",
+        messages = [
+            {"role": "system", "content": sys_msg},
+            {"role": "user", "content": user_msg}
+        ],
+        temperature = 0.3,
+    )
+    
+    return completion.choices[0].message.content
+
+
+
+
 def main():
     try:
         # 获取用户姓名和邮箱
@@ -33,7 +67,7 @@ def main():
         subprocess.run(['git', 'add', '-A'])
 
         # 提交到本地仓库
-        subprocess.run(['git', 'commit', '-m', final_commit_message])
+        subprocess.run(['git', 'commit', '-m', kimi_robot(final_commit_message)])
 
         # 推送到远程仓库
         subprocess.run(['git', 'push'])
